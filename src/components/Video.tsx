eb4940446ff6f670b0aa26b5dae4eb508cd6c1b6
Caio <kayoroxa@gmail.com>
2022-11-03 02:00:43 -0300

    .

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -5,15 +5,13 @@ interface Props {
 
 export default function Video({ id, className }: Props) {
   return (
-    <div className={className}>
-      <iframe
-        width="100%"
-        height="100%"
-        src={'https://www.youtube.com/embed/' + id}
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-        frameBorder={0}
-      ></iframe>
-    </div>
+    <iframe
+      width="100%"
+      height="100%"
+      src={'https://www.youtube.com/embed/' + id}
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowFullScreen
+      frameBorder={0}
+    ></iframe>
   )
 }
--- src/pages/course/[id].tsx
+++ src/pages/course/[id].tsx
@@ -1,12 +1,26 @@
+import { useState } from 'react'
 import Video from '../../components/Video'
-import { _Course, _Data } from '../../utils/@types/_Data'
+import { _Course, _Data, _Lesson } from '../../utils/@types/_Data'
 
 export default function Course({ course }: { course: _Course }) {
+  const [vidData, setVidData] = useState<_Lesson>({} as _Lesson)
+
   return (
-    <div className="flex flex-wrap p-6">
-      {course.lessons.map(v => (
-        <Video id={v.id} key={v.id} />
-      ))}
+    <div className="w-full min-h-screen flex justify-center gap-6 p-6 flex-wrap bg-zinc-900">
+      <section className="flex flex-wrap bg-black w-[600px] h-[280px] rounded-xl justify-center overflow-hidden">
+        <Video id={vidData.id || course.lessons[0].id} />
+        <p>Carregando...</p>
+      </section>
+      <section className="flex flex-[0.5] p-6 flex-col gap-5 bg-zinc-800 rounded-xl">
+        {course.lessons.map((v, i) => (
+          <div className="rounded-xl bg-zinc-600 p-2 hover:bg-red-600 hover:cursor-pointer transition-all hover:scale-105 flex gap-2 items-center">
+            <span className="bg-zinc-500 rounded-xl px-2 py-1">{i + 1}</span>
+            <div className=" " onClick={() => setVidData(v)}>
+              {v.name}
+            </div>
+          </div>
+        ))}
+      </section>
     </div>
   )
 }
