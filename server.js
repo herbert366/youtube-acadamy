12a3ddda7672ffe2b9dc826e6d9cc1e8460f0114
herbert366 <herbert.artur@gmail.com>
2023-05-19 14:14:30 -0300

    fix port back

--- server.js
+++ server.js
@@ -10,7 +10,10 @@ if (process.env.JSON_DB_PATH) {
   server.use(middlewares)
   server.use(myMiddlewares)
   server.use(router)
-  server.listen(process.env.BACK_PORT || 4000, () => {
-    console.log('JSON Server is running on port 4011')
+  server.listen(process.env.BACK_PORT || 4032, () => {
+    console.log(
+      'JSON Server is running on port:',
+      process.env.BACK_PORT || 4032
+    )
   })
 }
