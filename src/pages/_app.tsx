2eada5532b3d5701a09f96697c11c941f5e55290
herbert366 <herbert.artur@gmail.com>
2023-06-03 20:55:50 -0300

    faz parte da att anterior

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,10 +4,13 @@ import Link from 'next/link'
 import { BsYoutube } from 'react-icons/bs'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import '../styles/globals.css'
+import CreateButton from '../core/CreateButton'
+import { useCourses } from '../hooks/useCourses'
 
 export const queryClient = new QueryClient()
 
 export default function App({ Component, pageProps }: AppProps) {
+  const createCourse = useCourses().create
   return (
     <QueryClientProvider client={queryClient}>
       <Head>
@@ -17,18 +20,31 @@ export default function App({ Component, pageProps }: AppProps) {
       </Head>
 
       <header className="flex gap-4 p-4 justify-start items-center bg-zinc-800/50">
-        <Link href={'/'}>
-          <div className="flex justify-center items-center group">
-            <BsYoutube
-              color="white"
-              size={30}
-              className="group-hover:fill-red-500"
-            />
-            <h1 className="-ml-3 -mt-1 text-2xl font-bold group-hover:text-red-500 py-1 px-4">
-              YouCademy
-            </h1>
-          </div>
-        </Link>
+        <section className="flex gap-4">
+          {' '}
+          <Link href={'/'}>
+            <div className="flex justify-center items-center group">
+              <BsYoutube
+                color="white"
+                size={30}
+                className="group-hover:fill-red-500"
+              />
+              <h1 className="-ml-3 -mt-1 text-2xl font-bold group-hover:text-red-500 py-1 px-4">
+                YouCademy
+              </h1>
+            </div>
+          </Link>
+          <CreateButton
+            data={{
+              name: { initialValue: '', type: 'text' },
+              poster: { initialValue: '', type: 'text' },
+            }}
+            title="Add course"
+            onSubmit={v => {
+              createCourse(v)
+            }}
+          />
+        </section>
       </header>
       <Component {...pageProps} />
     </QueryClientProvider>
